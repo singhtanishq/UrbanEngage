73a0de676a99bc60bed23cbf7f5a874ac727c27e
Tanishq Singh <tanishqsingh@outlook.in>
2024-09-27 04:33:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,13 @@ const accountsRouter = require('./routes/accounts');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const URI = process.env.ATLAS_URI || '';
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(URI, 
+mongoose.connect('mongodb+srv://tanishqsingh:pswd123@cluster.qmdmnpx.mongodb.net/UrbanEngageDB?retryWrites=true&w=majority', 
     { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
